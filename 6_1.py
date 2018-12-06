01b212b633433a5216aa29f1d342f5f60e79a299
Eric Broersma <git@ericbroersma.nl>
2018-12-06 07:33:20 +0100

    Clean up day 6

--- 6_1.py
+++ 6_1.py
@@ -2,13 +2,15 @@ import little_helper
 
 from collections import defaultdict
 
+
 def distance(coord, x,y ):
     return abs(coord[0] - x) + abs(coord[1] - y)
 
+
 def is_finite(coord, coords):
     return coord[0] > min(c[0] for c in coords) and coord[1] > min(c[1] for c in coords) and coord[0] < max(c[0] for c in coords) and coord[1] < max(c[1] for c in coords)
-def is_nearer_to(coorda, coordb, x, y):
-    return distance(coorda, x, y) < distance(coordb, x,y)
+
+
 def answer(input):
     lines = input.split('\n')
 
@@ -18,7 +20,6 @@ def answer(input):
         x = int(x)
         y = int(y)
         coords.append((x,y))
-    
 
     minx = min(c[0] for c in coords) - 1
     miny = min(c[1] for c in coords) - 1
@@ -40,40 +41,13 @@ def answer(input):
                     min_dist_area = i
             areas[(x,y)] = min_dist_area
 
-    """
-    print(areas)
-    for y in range(miny, maxy):
-        for x in range(minx, maxx):
-            if areas[(x,y)] == -1:
-                print('.', end='')
-            else:
-                print(areas[(x,y)], end='')
-        print()
-    """
     area_size = defaultdict(int)
     for area in areas:
         if is_finite(area, coords):
             area_size[areas[area]] +=1
-    
-    #print(area_size)
 
     return max(area_size.values())
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('level', type=int, default=-1, nargs='?')
-    args = parser.parse_args()
-    level = args.level
-
-    day = 6
-
-    input = little_helper.get_input(day)
-    the_answer = answer(input)
-
-    if level == -1:
-        print(the_answer)
-    else:
-        print("Submitting", the_answer, "for day", day,"star", level)
-        print(little_helper.submit(the_answer, level, day))
+    print(answer(little_helper.get_input(6)))
