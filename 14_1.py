ecc3d5c949716c891ac878e9b2b4f3fb5622b6e1
Eric Broersma <git@ericbroersma.nl>
2018-12-14 07:37:56 +0100

    Clean up day 14

--- 14_1.py
+++ 14_1.py
@@ -1,72 +1,39 @@
 import little_helper
 
-# defaultdict(list), defaultdict(int), deque.rotate/append
-from collections import defaultdict, deque
-# functools.reduce(function, iterable[, initializer])
-from functools import reduce
-# islice(seq, [start,] stop [, step])
-from itertools import islice
-import re
-#import networkx as nx
-#from numba import jit
-from sys import exit
-
-day = 14
-if __file__.endswith("_2"):
-    m = __import__(day + "_1")
-import math
-
-def num_digits(n):
-    return len(str(n))#int(math.log10(n))+1
-
-def get_digit(number, n):
-    return number // 10**n % 10
 
 def answer(input):
-    """
-    >>> answer("1234")
-    1234
-    """
     input = int(input)
+
     recipes = [3,7]
     one = 0
     two = 1
     one_recipe = recipes[one]
     two_recipe = recipes[two]
-    while True:
-        new_recipe = one_recipe + two_recipe
-        
-        for i in range(num_digits(new_recipe))[::-1]:
-            recipes.append(get_digit(new_recipe, i))
 
-        if len(recipes) >= input + 10:
+    while True:
+        sum_recipes = one_recipe + two_recipe
+
+        new_recipe = sum_recipes // 10 % 10
+        if new_recipe > 0:
+            recipes.append(new_recipe)
+            
+        new_recipe = sum_recipes % 10
+        recipes.append(new_recipe)
+
+        num_recipes = len(recipes)
+
+        if num_recipes >= input + 10:
             break
-        one += (1+one_recipe)
-        one %= len(recipes) 
+
+        one = (one + 1 + one_recipe) % num_recipes
         one_recipe = recipes[one]
 
-        two += (1+two_recipe)
-        two %= len(recipes)
+        two = (two + 1 + two_recipe) % num_recipes
         two_recipe = recipes[two]
 
-
-        #print(one, one_recipe, two, two_recipe)
-    #print(recipes)
     return ''.join(str(n) for n in recipes[-10:])
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('level', type=int, default=-1, nargs='?')
-    args = parser.parse_args()
-    level = args.level
-
-    input = little_helper.get_input(day)
-    the_answer = answer(input)
-
-    if level == -1:
-        print(the_answer)
-    else:
-        print("Submitting", the_answer, "for day", day,"star", level)
-        print(little_helper.submit(the_answer, level, day))
+    input = little_helper.get_input(14)
+    print(answer(input))
--- 14_2.py
+++ 14_2.py
@@ -1,73 +1,42 @@
 import little_helper
 
-# defaultdict(list), defaultdict(int), deque.rotate/append
-from collections import defaultdict, deque
-# functools.reduce(function, iterable[, initializer])
-from functools import reduce
-# islice(seq, [start,] stop [, step])
-from itertools import islice
-import re
-#import networkx as nx
-#from numba import jit
-from sys import exit
-
-day = 14
-if __file__.endswith("_2"):
-    m = __import__(day + "_1")
-import math
-
-def get_digit(number, n):
-    return number // 10**n % 10
 
 def answer(input):
-    """
-    >>> answer("1234")
-    1234
-    """
-    input = int(input)
+    input = [int(c) for c in input]
+
     recipes = [3,7]
     one = 0
     two = 1
     one_recipe = recipes[one]
     two_recipe = recipes[two]
-    while True:
-        new_recipe = one_recipe + two_recipe
 
-        d = new_recipe // 10 % 10
-        if d > 0:
-            recipes.append(d)
+    while True:
+        sum_recipes = one_recipe + two_recipe
+
+        new_recipe = sum_recipes // 10 % 10
+        if new_recipe > 0:
+            recipes.append(new_recipe)
             
-        d = new_recipe % 10
-        recipes.append(d)
+        new_recipe = sum_recipes % 10
+        recipes.append(new_recipe)
 
         num_recipes = len(recipes)
-        one = (one +1+one_recipe)%num_recipes
+
+        if num_recipes > len(input) + 1:
+            if recipes[-(len(input)+1):-1] == input:
+                return num_recipes - len(input) + 1
+            elif recipes[-len(input):] == input:
+                return num_recipes - len(input)
+
+        one = (one + 1 + one_recipe) % num_recipes
         one_recipe = recipes[one]
 
-        two = (two+1+two_recipe)%num_recipes
+        two = (two + 1 + two_recipe) % num_recipes
         two_recipe = recipes[two]
 
-        if recipes[-2] == 1 and recipes[-3] == 2 and recipes[-4] == 0 and recipes[-5] == 6 and recipes[-6] == 3 and recipes[-7] == 2:
-            return len(recipes) - 7
-        if recipes[-1] == 1 and recipes[-2] == 2 and recipes[-3] == 0 and recipes[-4] == 6 and recipes[-5] == 3 and recipes[-6] == 2:
-            return len(recipes) - 6
-
-        #print(one, one_recipe, two, two_recipe)
     return -1
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('level', type=int, default=-1, nargs='?')
-    args = parser.parse_args()
-    level = args.level
-
-    input = little_helper.get_input(day)
-    the_answer = answer(input)
-
-    if level == -1:
-        print(the_answer)
-    else:
-        print("Submitting", the_answer, "for day", day,"star", level)
-        print(little_helper.submit(the_answer, level, day))
+    input = little_helper.get_input(14)
+    print(answer(input))
